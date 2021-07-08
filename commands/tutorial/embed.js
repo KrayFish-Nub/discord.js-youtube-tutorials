6940b91b7d598d524360777d6fe7a3243ac7902f
pritulx HQ <contact.pritulx@gmail.com>
2021-07-08 15:17:41 +0530

    Create embed.js

---
+++ commands/tutorial/embed.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js')
+const MessageEmbed = require('discord.js')
+const { Message, Client } = require('discord.js')
+
+client.on('message', () => {
+  if(message.content === 'yembed') {
+  return message.channel.send(
+  new MessageEmbed
+    .setTitle(`hello ${message.author.name}`)
+    .setDescription(`Hello world this is embed description`)
+    .setFooter(`${message.author.tag}`, message.author.displayAvatarURl())
+    .setThumbnail(`https://image-url.....`)
+    .setImage(`https://image-url.....`)
+    .setTimpestamp()
+    .setAuthor(`${message.author.tag}`, message.author.displayAvatarURl())
+    .addField(`Field name`, `Filed description`, true)
+    
+    //OR
+    
+    .addFields([
+      
+      {
+        name: "Field1",
+        value: "Field1 description",
+        inline: true,
+      },
+      {
+        name: "Field2",
+        value: "Field2 description",
+        inline: true,
+      },
+      {
+        name: "Field3",
+        value: "Field3 description",
+        inline: true,
+      },
+    
+    ])
+    
+  )
+  }
+})
