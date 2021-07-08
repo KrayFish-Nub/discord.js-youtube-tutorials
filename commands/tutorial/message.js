5fdf7dbaee5c4ffe55bc5a1a2b173dbbc111a143
pritulx HQ <contact.pritulx@gmail.com>
2021-07-08 15:19:39 +0530

    Update message.js

--- commands/tutorial/message.js
+++ commands/tutorial/message.js
@@ -1,1 +1,13 @@
-sdf
+// Make sure re-edit code as your need!!
+
+const Discord = require('discord.js')
+const MessageEmbed = require('discord.js')
+const { Message, Client } = require('discord.js')
+
+client.on('message', () => {
+  if(message.content === 'yembed') {
+  return message.channel.send(`Hello ${message.author,tag}`)
+    
+//     or
+  return message/reply(`Hello ${message.author.tag}`)
+  }
